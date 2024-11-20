96e1d03513247e619b0f718e0adfd19d95db90f6
DavN24 <94380629+DavN24@users.noreply.github.com>
2024-11-20 10:39:24 +0100

    Update decoder.py

--- FaderNetwork/Models/decoder.py
+++ FaderNetwork/Models/decoder.py
@@ -12,10 +12,10 @@ class Decoder(nn.Module) :
 
         super(Decoder, self).__init__()
         
-        # On code l'architecture
+        # On code l'architecture en suivant les paramètres pris par l'article comme la taille du kernel, du stride...
         # Architecture symétrique au Fader Encoder et on utilise une entrée de dim + 2n avec n le nbre d'attribut
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(dim + 2 * attribut, 512, kernel_size=4, stride=2, padding=1),  # Up-sampling
+            nn.ConvTranspose2d(dim + 2 * attribut, 512, kernel_size=4, stride=2, padding=1),  
             nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
             
@@ -43,16 +43,20 @@ class Decoder(nn.Module) :
             nn.Tanh()  # Normalisation de la sortie dans [-1, 1]
         )
 
-
+    # On definition le fonction forward qui définit le déroulé dans le décodeur  
     def forward(self, latent_code, attributes):
        
         # On concaténe les attributs codés en one-hot au latent_code
         # Le but est d'avoir un vecteur [1, 0] ou [0, 1]
         attributes_onehot = torch.cat([attributes, 1 - attributes], dim=1) 
+        
+        # On rajoute des dimensions spatiales comme la hauteur et la largeur representé par (dim = 2 et 3) 
         attributes_onehot = attributes_onehot.unsqueeze(2).unsqueeze(3)  
+
+        # On fait "expand" pour faire correspondre les dimensions aux dimensions du latent code (qui est la sortie du encodeur)
         attributes_onehot = attributes_onehot.expand(-1, -1, latent_code.size(2), latent_code.size(3))
         
-        # On ajoute les attributs au code latent
+        # On ajoute les attributes_onehot qu'on a adapté au code latent (sortie de l'encodeur)
         decoder_input = torch.cat([latent_code, attributes_onehot], dim=1)  
         
         # On passe dans le décodeur
