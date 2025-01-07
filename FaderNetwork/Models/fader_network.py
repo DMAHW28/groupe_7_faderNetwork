7cde9739e38579217b898456be82c3b7be1e35e4
Khouzema EMADALY <129859741+Khouz360@users.noreply.github.com>
2025-01-07 10:44:15 +0100

    Update fader_network.py

--- FaderNetwork/Models/fader_network.py
+++ FaderNetwork/Models/fader_network.py
@@ -3,11 +3,11 @@ from .encoder import Encoder
 from .decoder import Decoder
 
 class FaderNetwork(nn.Module):
-    def __init__(self, input_channels=3, latent_channels=512, attribute_dim=40):
+    def __init__(self, input_channels=3, latent_channels=512, attribute_dim=40, attributes=[]):
         super(FaderNetwork, self).__init__()
-        self.encoder = Encoder(input_channels, latent_channels)
+        self.attributes = attributes
+        self.encoder = Encoder(latent_channels, input_channels)
         self.decoder = Decoder(latent_channels, attribute_dim)
-        # self.decoder = Decoder(latent_channels, attribute_dim, input_channels)
     
     def forward(self, x, attributes):
         latent = self.encoder(x)  # [batch_size, 512, 2, 2]
