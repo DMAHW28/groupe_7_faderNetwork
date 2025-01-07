03d3668130294c31e2213cd3b3d128c3c2ba8ec5
Khouzema EMADALY <129859741+Khouz360@users.noreply.github.com>
2025-01-07 10:33:36 +0100

    Update decoder.py

--- FaderNetwork/Models/decoder.py
+++ FaderNetwork/Models/decoder.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 
 # On crée la classe Décoder
 
-class Decoder(nn.Module) :
+"""class Decoder(nn.Module) :
     def __init__(self, dim = 512, attribut = 10) :
 
         super(Decoder, self).__init__()
@@ -62,23 +62,57 @@ class Decoder(nn.Module) :
 
         # On passe dans le décodeur
         output = self.decoder(decoder_input)
+        return output"""
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim=512, num_attributes=3):
+        super(Decoder, self).__init__()
+        self.num_attributes = num_attributes
+        self.attribute_dim = 2*num_attributes
+        self.input_dim = latent_dim + self.attribute_dim
+
+        self.dec1 = self.layer_decoder(canal=self.input_dim , filter_size=512)
+        self.dec2 = self.layer_decoder(canal=512 + self.attribute_dim, filter_size=512)
+        self.dec3 = self.layer_decoder(canal=512 + self.attribute_dim, filter_size=256)
+        self.dec4 = self.layer_decoder(canal=256 + self.attribute_dim, filter_size=128)
+        self.dec5 = self.layer_decoder(canal=128 + self.attribute_dim , filter_size=64)
+        self.dec6 = self.layer_decoder(canal=64 + self.attribute_dim, filter_size=32)
+        self.dec7 = torch.nn.Sequential(
+            torch.nn.ConvTranspose2d(32 + self.attribute_dim, 3, kernel_size=4, stride=2, padding=1),
+            torch.nn.Tanh()
+        )
+        
+    def forward(self, latent, attributes):
+        batch_size = latent.size(0)
+        attributes = attributes.unsqueeze(2).unsqueeze(3)
+        input_1 = torch.cat([latent, attributes.expand(batch_size, self.attribute_dim, latent.size(2), latent.size(2))], 1)
+
+        dec1 = self.dec1(input_1)
+        input_2 = torch.cat([dec1, attributes.expand(batch_size, self.attribute_dim, dec1.size(2), dec1.size(2))], 1)
+        
+        dec2 = self.dec2(input_2)
+        input_3 = torch.cat([dec2, attributes.expand(batch_size, self.attribute_dim, dec2.size(2), dec2.size(2))], 1)
+        
+        dec3 = self.dec3(input_3)
+        input_4 = torch.cat([dec3, attributes.expand(batch_size, self.attribute_dim, dec3.size(2), dec3.size(2))], 1)
+        
+        dec4 = self.dec4(input_4)
+        input_5 = torch.cat([dec4, attributes.expand(batch_size, self.attribute_dim, dec4.size(2), dec4.size(2))], 1)
+        
+        dec5 = self.dec5(input_5)
+        input_6 = torch.cat([dec5, attributes.expand(batch_size, self.attribute_dim, dec5.size(2), dec5.size(2))], 1)
+        
+        dec6 = self.dec6(input_6)
+        input_7 = torch.cat([dec6, attributes.expand(batch_size, self.attribute_dim, dec6.size(2), dec6.size(2))], 1)
+        output = self.dec7(input_7)
         return output
+    
+    def layer_decoder(self, canal, filter_size):
+        return torch.nn.Sequential(
+            torch.nn.ConvTranspose2d(canal, filter_size, kernel_size=4, stride=2, padding=1),
+            torch.nn.BatchNorm2d(filter_size),
+            torch.nn.ReLU(inplace=True)
+        )
 
 
-if __name__ == "__main__":
-    # Paramètres
-    latent_dim = 512
-    num_attributes = 10
-    batch_size = 32
-    image_size = 256
 
-    # Input provisoire
-    latent_code = torch.randn(batch_size, latent_dim, 2, 2)  # Taille latente
-    attributes = torch.randint(0, 2, (batch_size, num_attributes))  # Attributs binaires
-
-    # Initialisation du décodeur
-    decoder = Decoder(dim=latent_dim, attribut=num_attributes)
-
-    # Génération de l'image
-    reconstructed_images = decoder(latent_code, attributes)
-    print(f"Taille des images reconstruites : {reconstructed_images.shape}")  
